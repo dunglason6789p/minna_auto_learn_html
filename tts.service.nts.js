d150be7c334752510678ca12b1a742aab4dad18c
sonnguyen2djv <son.nguyen2@drjoy.vn>
2021-03-26 15:57:45 +0900

    6b--minor

---
+++ tts.service.nts.js
@@ -0,0 +1,35 @@
+let URL_TTS_SERVER = 'http://localhost:8080'; // Allow editing url!
+function textToSpeech(textInput) {
+  if (!textInput) {
+    alert('Empty input!')
+  } else {
+    const urlTTS = URL_TTS_SERVER+'/tts'
+    fetch(urlTTS, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: textInput,
+      }) // body data type must match "Content-Type" header
+    }).then(async response => {
+      console.log('response=<below>');
+      console.log(response);
+      const responseAsJson = await response.text();
+      console.log(`responseAsJson=${responseAsJson}`);
+      const audioUrl = URL_TTS_SERVER + responseAsJson.replace('audio/','/my-audio/');
+      playAudioFromUrl(audioUrl);
+    }).catch(error => {
+      console.error('error=<below>');
+      console.error(error);
+    });
+  }
+}
+function playAudioFromUrl(audioUrl) {
+  const audioElm = new Audio(audioUrl);
+  audioElm.play().then(audioPlayResult => {
+    console.log(`Played audio from URL:${audioUrl}`);
+  }).catch(audioPlayError => {
+    console.warn(`Cannot play audio from URL:${audioUrl}`);
+  });
+}
